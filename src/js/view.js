134e1fa41c55c3be109fa8e76a93c5fd5b928250
Kento Nakamura <kento.nakamura2009@gmail.com>
2021-10-23 13:36:38 +0900

    撮影地メモ機能を廃止・画像を角丸に変更・リンクの表示文字列を変更可能にする機能を実装

--- src/js/view.js
+++ src/js/view.js
@@ -2,6 +2,8 @@
 const addPicture = document.getElementById("add-picture");
 const pictureDivided = document.getElementById('add-picture-area');
 const linkText = document.getElementById('linkText');
+const attention = document.getElementById('attention');
+const inLinkText = document.getElementById('Text');
 let linkValue;
 
 // 画像が追加された時の処理
@@ -9,7 +11,9 @@ addPicture.addEventListener("change", function(evt){
   const file = evt.target.files;
   const reader = new FileReader();
   linkValue = linkText.value
-  console.log(linkValue)
+  inLinkValue = inLinkText.value
+  console.log(linkValue);
+  console.log(inLinkValue);
 
   //dataURL形式でファイルを読み込む
   reader.readAsDataURL(file[0]);
@@ -17,12 +21,15 @@ addPicture.addEventListener("change", function(evt){
   //ファイルの読込が終了した時の処理
   reader.onload = function(){
   const dataUrl = reader.result;
+  attention.innerText = '他の写真も追加できます';
 
   //読み込んだ画像とdataURLを書き出す
   if (linkValue.length === 0) {
-    pictureDivided.insertAdjacentHTML("beforebegin" ,"<div class='img-div'><img src='" + dataUrl + "'><br><textarea placeHolder='メモを入力' class='memo'></textarea><br><textarea placeHolder='撮影地を入力' class='memo'></textarea><br><strong>リンク指定なし</strong></div>");
+    pictureDivided.insertAdjacentHTML("beforebegin" ,"<div class='img-div'><img src='" + dataUrl + "'><br><textarea placeHolder='メモを入力' class='memo'></textarea><br><strong>リンク指定なし</strong></div>");
+  } else if (inLinkValue.length === 0) {
+    pictureDivided.insertAdjacentHTML("beforebegin" , "<div class='img-div'><img src='" + dataUrl +  "'><br><textarea placeHolder='メモを入力'  class='memo'></textarea><br><a href='" + linkValue +"' target='_blank' rel='noopener   noreferrer'>リンク</a></div>");
   } else {
-    pictureDivided.insertAdjacentHTML("beforebegin" ,"<div class='img-div'><img src='" + dataUrl + "'><br><textarea placeHolder='メモを入力' class='memo'></textarea><br><textarea placeHolder='撮影地を入力' class='memo'></textarea><br><a href='" + linkValue +"'>リンク</a></div>");
-  }
+    pictureDivided.insertAdjacentHTML("beforebegin" , "<div class='img-div'><img src='" + dataUrl +  "'><br><textarea placeHolder='メモを入力'  class='memo'></textarea><br><a href='" + linkValue +"' target='_blank' rel='noopener   noreferrer'>" + inLinkValue +"</a></div>");
+    }
   }
 },false);
